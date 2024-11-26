fb102913ebdac096b62a0a172ed4bf87fd655efa
Ifrah Pervaiz <ifrahsyed529@gmail.com>
2024-11-27 00:23:35 +0500

    added input area

---
+++ app.py
@@ -0,0 +1,9 @@
+#for career suggestion
+print("We are here to suggest you the best for your career.")
+print("You have to give answer of few questions.")
+while True:
+    field=input("Enter your intermediate subject (pre):").lower()
+    interest=input("Enter your area of interest:").lower()
+    will= input("Are you willing to work in different locations? (yes/no): ").lower()
+    salaryexp=input("What is your salary expectation (low, medium, high)? ").lower()
+    suggestions = 0
